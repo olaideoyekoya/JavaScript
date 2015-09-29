2751288ca0c2184ede586ce2c4d19537fb8f0b2f
Olaide Oyekoya <olaideoyekoya@gmail.com>
2015-09-28 23:41:53 -0400

    - Added freeform element to interview.html - Added calculateFreeform function - Added validateOperand function

--- Exercises-JavaScript/src/plainjs.js
+++ Exercises-JavaScript/src/plainjs.js
@@ -19,7 +19,10 @@ var Equation = function() {
     this.answer   = null;
 };
 
-Equation.prototype.compute = function() {
+Equation.prototype.compute = function(answerId) {
+    this.validateOperand(this.operand1, "operand1");
+    this.validateOperand(this.operand2, "operand2");
+
     switch (this.operator) {
     case '+':
         this.answer = this.operand1 + this.operand2;
@@ -37,27 +40,67 @@ Equation.prototype.compute = function() {
     default:
         break;
     } 
-    $('#answer').text(this.answer);
+
+    $('#answer'+answerId).text(this.answer);
 };
 
 Equation.prototype.updateOperand = function(event) {
-	if (event.currentTarget.id === 'operand1')
+    if (event.currentTarget.id === 'operand1') {
         this.operand1 = parseFloat($(event.currentTarget).val());
-    else 
-		this.operand2 = parseFloat($(event.currentTarget).val());
-	this.operator = $("#operator option:selected").text();		
-    this.compute();
+    }
+    else {
+        this.operand2 = parseFloat($(event.currentTarget).val());
+    }
+    this.operator = $("#operator option:selected").text();		
+    this.compute(1);
 };
 
 Equation.prototype.updateOperator = function(event) {
     this.operator = $(event.currentTarget).val();
-    this.compute();
+    this.compute(1);
 };
 
+// Validate our operands 
+Equation.prototype.validateOperand = function(operand, operandId) {
+    if ( isNaN(operand) ){
+        alert("The operand is not a valid number!");
+        $("#"+operandId).val("");
+        $("#"+operandId).focus()
+    }
+};
+
+//Parse free-form text into 2 operands and an operator
+Equation.prototype.calculateFreeform = function(event) {
+    var entry = $(event.currentTarget).val();
+    if ( entry.match(/\+/) ) {
+        this.operator = '+';
+    }
+    else if ( entry.match(/\-/) ) {
+        this.operator = '-';
+    }
+    else if ( entry.match(/\//) ) {
+        this.operator = '/';
+    }
+    else if ( entry.match(/\*/) ) {
+        this.operator = '*';
+    }
+    else {
+        alert("The equation '"+entry+"' is not valid.");
+    }
+
+    var operands  = entry.split(this.operator); 
+    this.operand1 = parseFloat(operands[0]);
+    this.operand2 = parseFloat(operands[1]);
+    this.compute(2);
+};
+
+// Create an Equation object when the page loads
 window.onload = function() {
     // WARNING: don't treat equation as a global variable in any changes you make
     var equation = new Equation();
-	
+
+    // Ensure that our new object is passed along with the event object to the function
     $('#operator').change(function(e) { equation.updateOperator(e); });
-	$('.operand').change(function(e) { equation.updateOperand(e); });
+    $('.operand').change(function(e) { equation.updateOperand(e); });
+    $('#freeform').change(function(e) { equation.calculateFreeform(e); });
 };
